0b7c002f074f83c63a2e445b7ff982f17898d19b
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-09-15 15:34:57 +0300

    Add files via upload

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,55 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+	ASSERT_DOUBLE_EQ(2.5, average(3.0, 2.0));
 
+}
+TEST(task1, test2)
+{
+	ASSERT_DOUBLE_EQ(1.0, average(1.5, 0.5));
+
+}
+TEST(task1, test3)
+{
+	ASSERT_DOUBLE_EQ(false, good(1, 0));
+
+}
+TEST(task1, test4)
+{
+	ASSERT_DOUBLE_EQ(true, good(1, 0.999999999));
+
+}
+
+TEST(task1, test5)
+{
+	ASSERT_DOUBLE_EQ(2.0, improve(3.0, 3.0));
+
+}
+
+TEST(task1, test6)
+{
+	ASSERT_DOUBLE_EQ(1, iter(1, 0.999999999));
+
+}
+TEST(task1, test7)
+{
+	ASSERT_DOUBLE_EQ(4.0, iter(4.0, 16.0));
+
+}
+TEST(task1, test8)
+{
+	ASSERT_NEAR(2.64575131, calc(7.0), 0.000001);
+
+}
+TEST(task1, test9)
+{
+	ASSERT_NEAR(3.0, calc(9.0), 0.000001);
+
+}
+TEST(task1, test10)
+{
+	ASSERT_NEAR(2.23606798, calc(5.0), 0.000001);
+
+}
