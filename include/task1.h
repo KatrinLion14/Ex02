421c26fe70e38335b9e3fa2bc40ba7ae8a44deb8
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-09-15 15:21:43 +0300

    Add files via upload

---
+++ include/task1.h
@@ -0,0 +1,10 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif
